ae11d9e38d084118c0016ebb739e0370d57e0b94
teddyh84 <teddyh84@gmail.com>
2024-11-26 14:58:10 +0100

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Bienvenue sur mon application")
 st.write("Ceci est une application Streamlit simple.")
+
+
+premier_nombre = int(input("Premier nombre ?"))
+deuxieme_nombre = int(input("Deuxième nombre ?"))
+result_adi = premier_nombre + deuxieme_nombre
+print("Le resultat de l'addition est " + str(result_adi))
