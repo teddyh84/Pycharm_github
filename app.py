c17eded96123ee2e1a70535c7e291ae6cba24b98
teddy <teddyh84@gmail.com>
2024-12-01 00:18:13 +0100

    Init

--- app.py
+++ app.py
@@ -3,24 +3,25 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    message = ""
+    erreur = ""
+    num1 = ""
+    num2 = ""
+    result = ""
+    if request.method == 'POST':
+        if 'calculer' in request.form:
+            try:
+                # Récupérer les valeurs des champs du formulaire
+                num1 = int(request.form['num1'])
+                num2 = int(request.form['num2'])
+                result = num1 + num2
+                message = "Le résultat est : "
+            except ValueError:
+                erreur = "Erreur : veuillez entrer des nombres valides."
 
-
-@app.route('/add', methods=['POST'])
-def add():
-    try:
-        # Récupérer les valeurs des champs du formulaire
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
-        result = num1 + num2
-    except ValueError:
-        # Gérer les erreurs de conversion
-        return render_template('result.html', result="Erreur : veuillez entrer des nombres valides.")
-
-    return render_template('result.html', result=f"Le résultat est : {result}")
-
+    return render_template('index.html', message=message, erreur=erreur, result=result, num1=num1, num2=num2)
 
 if __name__ == '__main__':
     app.run(debug=True)
